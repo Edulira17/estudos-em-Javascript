aa9a05cb04bb6b41e31b3c3821e515d87cb48c25
Eduardo Lira <eduardoliracunha17@hotmail.com>
2024-06-27 22:32:02 -0400

    add: criando proprio metodo reduce usando Array.prototype && Imperativo e Declarativo && metodo 'concat'

---
+++ Arrays/concat.js
@@ -0,0 +1,9 @@
+// Ideia que voce consiga concatenar arrays em um novo array
+
+const filhas = ['Ualeska', 'Cibalena']
+const filhos = ['Uoxiton', 'Uesclei']
+
+const todos = filhas.concat(filhos)
+console.log(todos, filhas, filhos)
+
+console.log([].concat([1, 2],[3, 4], 5, [[6, 7]]))
